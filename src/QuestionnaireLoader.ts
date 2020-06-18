7a6b90ddf20923684b4781d6a996d9ee8ce19957
pmadria <pmadria@mitre.org>
2020-06-18 16:49:18 -0500

    updated files for questionnaire loader and test as well as questionnaire folder

--- src/QuestionnaireLoader.ts
+++ src/QuestionnaireLoader.ts
@@ -1,11 +1,49 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
+import * as fs from 'fs';
+//import * as util from 'util'
+import { IQuestionnaire } from '@ahryman40k/ts-fhir-types/lib/R4';
+import axios from 'axios';
+
 
 export class QuestionnaireLoader {
   getFromFile(filePath: string): R4.IQuestionnaire {
     // use fs to read in file content as JSON
+    let obj = {} as IQuestionnaire;
+    const json = fs.readFileSync(`../Questionnaires/${filePath}`, 'utf8');
+    if (json) {
+      obj = JSON.parse(json);
+      console.log(obj);
+      return obj;
+    } else {
+      console.log('This is null or undefined');
+      return obj;
+    }
   }
 
   getFromUrl(url: string): R4.IQuestionnaire {
+    let obj = {} as IQuestionnaire;
     // use axios to send GET request and return response data
+    axios.get(url)
+    .then(function(response) {
+      console.log(response.status);
+      if (response.status == 200) {
+        obj = response.data;
+        console.log(obj);
+      }
+    })
+    .catch( function (error) {
+      if (error.response) {
+        console.log('The error status: ' + error.response.status);
+        console.log(error.response.headers);
+        return obj;
+      }
+    });
+    return obj;
   }
 }
+
+
+//
+//let QuestionnaireLoader1 = new QuestionnaireLoader();
+//QuestionnaireLoader1.getFromFile('sample_questionnaire.json');
+//QuestionnaireLoader1.getFromUrl('http://hapi.fhir.org/baseR4/Questionnaire/1221728');
--- test/QuestionnaireLoader.test.ts
+++ test/QuestionnaireLoader.test.ts
@@ -1,1 +1,16 @@
+import nock from 'nock';
+import {example_questionnaire} from '../Questionnaires/sample_questionnaire.json';
+import {getfromurl} from '../src/QuestionnaireLoader';
 // TODO: Add tests for QuestionnaireLoader class functions
+
+const MOCK_URL = 'http://example.com';
+
+test('questionnaire form yielding a questionnaire form result', async () => {
+    nock(MOCK_URL)
+        .get('/')
+        .reply(200, example_questionnaire);
+    
+    const actualQuestionnaire = await getfromurl(MOCK_URL);
+
+    expect(actualQuestionnaire).toEqual(example_questionnaire);
+});
