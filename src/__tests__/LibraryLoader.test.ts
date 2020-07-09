09a5f8edad00b3e3b945bc8ff044717ea8ef79d2
pmadria <pmadria@mitre.org>
2020-07-09 14:33:53 -0500

    finished unit test and libraryloader functionality

--- src/__tests__/LibraryLoader.test.ts
+++ src/__tests__/LibraryLoader.test.ts
@@ -1,39 +1,29 @@
 import nock from 'nock';
-import samplecql from './fixtures/sample-library-text-cql.json';
+import library_with_cql from './fixtures/sample-library-text-cql.json';
 import { LibraryLoader } from '../loaders/libraryLoader';
-import sampleelm from './fixtures/sample-library-elm.json';
+import library_with_elm from './fixtures/sample-library-elm.json';
 import { R4 } from '@ahryman40k/ts-fhir-types';
-
+import sampleelm from './fixtures/sample_known_elm.json';
+import { elmServiceUrl } from '../config.json';
 
 //Test for Library class function checkElm
-const MOCK_URL = 'http://example.com/cql/translator';
-// const MOCK_CQL = `
-// 'library example version '0.0.1'
-// using FHIR version '4.0.0'
-// codesystem "LOINC": 'http://loinc.org'
-// valueset "Cancer Disease Status Evidence Type Value Set": 'mcode-cancer-disease-status-evidence-type-vs'
-// code "CancerDiseaseStatus Code": '88040-1' from "LOINC"
-// define "CancerDiseaseStatus":
-//     [Observation: "CancerDiseaseStatus Code"]`;
-
-//library_cql = <R4.ILibrary> (JSON.parse(fs.readFileSync('./fixtures/sample-library-text-cql.json', 'utf8')));
-const library_cql = <R4.ILibrary> samplecql;
+const library_cql = <R4.ILibrary> library_with_cql;
 const LibraryLoader_cql = new LibraryLoader(library_cql);
 
-const library_elm = <R4.ILibrary> sampleelm;
+const library_elm = <R4.ILibrary> library_with_elm;
 const LibraryLoader_elm = new LibraryLoader(library_elm);
 
 test('correctly parse the cql into elm through translation service', async () => {
-  nock(MOCK_URL).get('/').reply(200, sampleelm);
+  nock(elmServiceUrl).post('/').reply(200, sampleelm);
 
   const actualElm = await LibraryLoader_cql.checkELM();
 
   expect(actualElm).toEqual(sampleelm);
 });
 
-//Test for QuestionnaireLoader class function getFromFile
+//Test for LibraryLoader class with the elm already in the data field
 test('correctly obtain the elm item', async () => {
-  const actualElm = LibraryLoader_elm.checkELM();
+  const actualElm = await LibraryLoader_elm.checkELM();
 
   expect(actualElm).toEqual(sampleelm);
 });
--- src/loaders/libraryLoader.ts
+++ src/loaders/libraryLoader.ts
@@ -6,14 +6,12 @@ import { convertBasicCQL } from '../helpers/cql-to-elm';
 
 export class LibraryLoader {
   library: R4.ILibrary;
-  //valueset: R4.IBundle;
 
   constructor(thelibrary: R4.ILibrary) {
     this.library = thelibrary;
-    //this.valueset = thevalueset;
   }
 
-  checkELM(): any {
+  async checkELM(): Promise<any> {
     //getting the elm and decoding the data components
     const contentInfoElm = this.library.content?.find(x => x.contentType === 'application/elm+json');
     const contentInfoTranslate = this.library.content?.find(x => x.contentType === 'text/cql');
@@ -21,14 +19,8 @@ export class LibraryLoader {
       return JSON.parse(Base64.decode(contentInfoElm.data));
      } else if (contentInfoTranslate && contentInfoTranslate.data) {
        //this is running the cql through a translation service and returning the elm if it isn't provided
-      return convertBasicCQL(contentInfoTranslate.data);
+        const decoded = Base64.decode(contentInfoTranslate.data);
+        return await convertBasicCQL(decoded);
     }
   }
-
-  //add valueset function later
 }
-
-// const library = <R4.ILibrary> (JSON.parse(fs.readFileSync('../__tests__/fixtures/sample-library.json', 'utf8')));
-// const LibraryLoader1 = new LibraryLoader(library);
-// const elm = LibraryLoader1.checkELM();
-// console.log(elm);
