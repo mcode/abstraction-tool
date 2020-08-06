38fd7bcf58bc00929d2469d5b3b4f8c35c68169c
Matthew Gramigna <mgramigna@mitre.org>
2020-08-06 14:22:43 -0400

    prettier

--- src/loaders/ValueSetLoader.ts
+++ src/loaders/ValueSetLoader.ts
@@ -1,6 +1,6 @@
-import { R4 } from "@ahryman40k/ts-fhir-types";
-import _ from "lodash";
-import { ValueSetMap } from "../types/valueset";
+import { R4 } from '@ahryman40k/ts-fhir-types';
+import _ from 'lodash';
+import { ValueSetMap } from '../types/valueset';
 
 export class ValueSetLoader {
   library: R4.ILibrary;
@@ -14,27 +14,23 @@ export class ValueSetLoader {
   seedValueSets(): ValueSetMap {
     const map: ValueSetMap = {};
     const valueSetIDs = _.flatten(
-      (this.library.dataRequirement ?? []).map((d) =>
-        d.codeFilter?.filter((cf) => cf.valueSet).map((cf) => cf.valueSet)
-      )
+      (this.library.dataRequirement ?? []).map(d => d.codeFilter?.filter(cf => cf.valueSet).map(cf => cf.valueSet))
     );
-    valueSetIDs.forEach((id) => {
-      const matchingEntry = this.contextBundle.entry?.find(
-        (e) => e.resource?.id === id
-      );
+    valueSetIDs.forEach(id => {
+      const matchingEntry = this.contextBundle.entry?.find(e => e.resource?.id === id);
       if (matchingEntry) {
         const resource = matchingEntry.resource as R4.IValueSet;
         if (resource.id && resource.version && resource.compose) {
           const codes = _.flatten(
-            resource.compose.include.map((i) => {
-              return (i.concept ?? []).map((c) => ({
+            resource.compose.include.map(i => {
+              return (i.concept ?? []).map(c => ({
                 code: c.code,
-                system: i.system ?? "",
+                system: i.system ?? ''
               }));
             })
           );
           map[resource.id] = {
-            [resource.version]: codes,
+            [resource.version]: codes
           };
         }
       }
