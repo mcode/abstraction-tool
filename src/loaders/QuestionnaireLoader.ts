6df1aeb5c004306819e6f7c9e3a8e49e3c5f0030
pmadria <pmadria@mitre.org>
2020-08-17 11:14:23 -0500

    obtains valueset data via url

--- src/loaders/QuestionnaireLoader.ts
+++ src/loaders/QuestionnaireLoader.ts
@@ -1,16 +1,16 @@
-import { R4 } from "@ahryman40k/ts-fhir-types";
-import * as fs from "fs";
-import axios from "axios";
+import { R4 } from '@ahryman40k/ts-fhir-types';
+import * as fs from 'fs';
+import axios from 'axios';
 
 export class QuestionnaireLoader {
   getFromFile(filePath: string): R4.IQuestionnaire {
     //accessing questionnaire folder for questionnaire specified by input
-    const json = fs.readFileSync(filePath, "utf8");
+    const json = fs.readFileSync(filePath, 'utf8');
     const obj = JSON.parse(json) as R4.IQuestionnaire;
-    if (obj && obj.resourceType === "Questionnaire") {
+    if (obj && obj.resourceType === 'Questionnaire') {
       return obj;
     } else {
-      throw new Error("provided file is not a valid FHIR questionnaire");
+      throw new Error('provided file is not a valid FHIR questionnaire');
     }
   }
 
--- src/loaders/ValueSetLoader.ts
+++ src/loaders/ValueSetLoader.ts
@@ -1,7 +1,7 @@
-import { R4 } from "@ahryman40k/ts-fhir-types";
-import _ from "lodash";
-import { ValueSetMap } from "../types/valueset";
-import FHIR from "fhirClient";
+import { R4 } from '@ahryman40k/ts-fhir-types';
+import _ from 'lodash';
+import { ValueSetMap } from '../types/valueset';
+import axios from 'axios';
 
 export class ValueSetLoader {
   library: R4.ILibrary;
@@ -15,27 +15,23 @@ export class ValueSetLoader {
   seedValueSets(): ValueSetMap {
     const map: ValueSetMap = {};
     const valueSetIDs = _.flatten(
-      (this.library.dataRequirement ?? []).map((d) =>
-        d.codeFilter?.filter((cf) => cf.valueSet).map((cf) => cf.valueSet)
-      )
+      (this.library.dataRequirement ?? []).map(d => d.codeFilter?.filter(cf => cf.valueSet).map(cf => cf.valueSet))
     );
-    valueSetIDs.forEach((id) => {
-      const matchingEntry = this.contextBundle.entry?.find(
-        (e) => e.resource?.id === id
-      );
+    valueSetIDs.forEach(id => {
+      const matchingEntry = this.contextBundle.entry?.find(e => e.resource?.id === id);
       if (matchingEntry) {
         const resource = matchingEntry.resource as R4.IValueSet;
         if (resource.id && resource.version && resource.compose) {
           const codes = _.flatten(
-            resource.compose.include.map((i) => {
-              return (i.concept ?? []).map((c) => ({
+            resource.compose.include.map(i => {
+              return (i.concept ?? []).map(c => ({
                 code: c.code,
-                system: i.system ?? "",
+                system: i.system ?? ''
               }));
             })
           );
           map[resource.id] = {
-            [resource.version]: codes,
+            [resource.version]: codes
           };
         }
       }
@@ -44,8 +40,7 @@ export class ValueSetLoader {
   }
 
   async getFromUrl(url: string): Promise<R4.IBundle> {
-    const client  = FHIR.client(url);
-    const response = await client.request(url);
+    const response = await axios.get(url);
     return response.data;
   }
 }
