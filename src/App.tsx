2f269e58047c415aa82d2522d443fcdb7efbaac3
pmadria <pmadria@mitre.org>
2020-08-21 11:03:33 -0500

    still working on rendering questionnaire

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { PatientProvider } from './components/PatientProvider';
 import Abstractor from './components/Abstractor';
+import QuestionnaireForm from "./components/QuestionnaireForm/questionnaireform";
+
 
 function App() {
   return (
     <PatientProvider>
       <Abstractor />
     </PatientProvider>
+    <QuestionnaireForm> 
+    </QuestionnaireForm>
   );
 }
 
