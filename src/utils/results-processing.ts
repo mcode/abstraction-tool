31305292fd5f8f0f91a258d945d0ddef1027dd50
mickohanlon23 <mohanlon@mitre.org>
2020-09-04 18:15:46 -0400

    Find all nonempty resources

--- src/utils/results-processing.ts
+++ src/utils/results-processing.ts
@@ -1,9 +1,23 @@
-export default function resultsProcessing(cqlResults: object): any {
+export default function resultsProcessing(cqlResults: any): any {
   // TODO: Process FHIR resources returned from CQL Execution
+
+  // Get each nonempty result
+  const patientResults = cqlResults.patientResults;
+  const patientID = Object.keys(patientResults)[0];
+  const igResources = cqlResults.patientResults[patientID];
+
+
+  for (let key in igResources) {
+    let resource = igResources[key];
+    //console.log(resource);
+    if (resource.length > 0){
+      console.log(key);
+      console.log(resource);
+    }
+  }
 
   // Get each nonempty result
   // Get name of expression
   // Match with linkID of questionnaire
-
-  return null;
+  return igResources;
 }
