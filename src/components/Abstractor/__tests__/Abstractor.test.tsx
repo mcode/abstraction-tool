5672db2c539f799285a2e50d1d85c9e897cb1f35
Matthew Gramigna <mgramigna@mitre.org>
2020-09-09 11:33:24 -0400

    Add mock util object

--- src/components/Abstractor/__tests__/Abstractor.test.tsx
+++ src/components/Abstractor/__tests__/Abstractor.test.tsx
@@ -3,5 +3,11 @@ import { render } from '@testing-library/react';
 import Abstractor from '../Abstractor';
 
 test('renders Abstractor', () => {
+  global.LForms = {
+    Util: {
+      convertFHIRQuestionnaireToLForms: jest.fn(),
+      addFormToPage: jest.fn()
+    }
+  };
   render(<Abstractor />);
 });
