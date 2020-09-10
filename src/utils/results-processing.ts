aa549df2d6c224f69c7e3d2a69f2d5a798057779
mickohanlon23 <mohanlon@mitre.org>
2020-09-10 15:17:48 -0400

    prettier

--- src/utils/results-processing.ts
+++ src/utils/results-processing.ts
@@ -1,10 +1,9 @@
-import { R4 } from "@ahryman40k/ts-fhir-types";
+import { R4 } from '@ahryman40k/ts-fhir-types';
 
 // Filter results by querying proper data from the returned FHIR resources.
 // Modify the answerOptions of the questionnaire to include the results from execution
 export default function questionnaireUpdater(cqlResults: any, questionnaire: R4.IQuestionnaire): R4.IQuestionnaire {
-
-  // Object Containing Questionnaire Items 
+  // Object Containing Questionnaire Items
   let questionnaireItems: any = questionnaire.item;
 
   // Get Non-Empty Patient Results
@@ -15,22 +14,18 @@ export default function questionnaireUpdater(cqlResults: any, questionnaire: R4.
   for (let key in igResources) {
     let resourceList = igResources[key];
     if (resourceList.length > 0) {
-
       // Accomodate for the return of multiple resources (for now this will just iterate over 1 resource)
       for (let fhirResource in resourceList) {
-
         // Match the FHIR Object to the Correspinding Questionnaire LinkID
         for (let r in questionnaireItems) {
-          let possibleMatch = questionnaireItems[r]["linkId"];
+          let possibleMatch = questionnaireItems[r]['linkId'];
           if (possibleMatch === key) {
-
             // Add as an answerOption for the Questionnaire item
             const answerOption = createAnswerOption(resourceList[fhirResource]);
 
             if (!questionnaireItems[r].answerOption) {
               questionnaireItems[r].answerOption = [answerOption];
-            }
-            else {
+            } else {
               questionnaireItems[r].answerOption.push(answerOption);
             }
           }
@@ -42,7 +37,7 @@ export default function questionnaireUpdater(cqlResults: any, questionnaire: R4.
   return questionnaire;
 
   function createAnswerOption(fhirObject: any) {
-    let referenceLocation = ( fhirObject._json.resourceType + "/" + fhirObject.id.value );
+    let referenceLocation = fhirObject._json.resourceType + '/' + fhirObject.id.value;
     // Format answer option
     const referenceObject = {
       valueReference: {
@@ -52,5 +47,4 @@ export default function questionnaireUpdater(cqlResults: any, questionnaire: R4.
     };
     return referenceObject;
   }
-
 }
