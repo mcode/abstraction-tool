4ff2ae01ee05cd835f3d7a07876f41bd077dd50a
mickohanlon23 <mohanlon@mitre.org>
2020-09-30 12:33:28 -0400

    Added entrypoint info to package.json

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@
 
 // Export Abstraction component
 
+export { default } from './components/Abstractor';
+
 // Export Loaders
+
+export { LibraryLoader } from './loaders/libraryLoader';
+export { QuestionnaireLoader } from './loaders/QuestionnaireLoader';
+export {ValueSetLoader } from './loaders/ValueSetLoader';
