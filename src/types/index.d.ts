edf769c5198fd7eba871c210d00b36c202df95b2
mickohanlon23 <mohanlon@mitre.org>
2020-10-02 11:23:38 -0400

    Remove redeclaration of modules

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,57 +1,8 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
 import { ValueSetMap } from './valueset';
 
-// ------ cql-exec.fhir.d.ts ----------------------
-
-declare module 'cql-exec-fhir' {
-  export const PatientSource = {
-    FHIRv400: class {
-      constructor(patient: R4.IBundle);
-      loadBundles(bundle: R4.IBundle): void;
-    }
-  };
-}
-
-// ------ cql-execution.d.ts ----------------------
-
-declare module 'cql-execution' {
-  export class Library {
-    constructor(elm: object, repo?: Repository);
-  }
-
-  export class Executor {
-    constructor(library: Library, codeService: CodeService);
-    exec(patientsource: PatientSource);
-  }
-
-  export class Repository {
-    constructor(elm: object);
-  }
-
-  export class CodeService {
-    constructor(valueSetsJson: ValueSetMap);
-  }
-}
-
-// ------ datasource.d.ts -------------------------
-
-export enum dataSourceTypes {
-  FILE = 'file'
-}
-
-// ------ valueset.d.ts ---------------------------
-
-export interface ValueSetMap {
-  [key: string]: {
-    [key: string]: R4.ICoding[];
-  };
-}
-
-// ------ window.t.ts -----------------------------
-
-export {};
-declare global {
-  interface Window {
-    LForms: any;
-  }
-}
+export * from './cql-exec-fhir';
+export * from './cql-execution';
+export * from './dataSource';
+export * from './valueset';
+export * from './window';
