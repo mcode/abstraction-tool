55fac5862fd603bbe5512b78fa9c24c02444757b
mickohanlon23 <mohanlon@mitre.org>
2020-10-02 12:04:09 -0400

    Move loader exports to src/loaders/index.ts

--- index.js
+++ index.js
@@ -2,6 +2,4 @@
 export { default } from './src/components/Abstractor';
 
 // Export Loaders
-export { LibraryLoader } from './src/loaders/libraryLoader';
-export { QuestionnaireLoader } from './src/loaders/QuestionnaireLoader';
-export { ValueSetLoader } from './src/loaders/ValueSetLoader';
+export * from './src/loaders';
