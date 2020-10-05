7fc643347282b92032b25a42466a3a1219018fed
mickohanlon23 <mohanlon@mitre.org>
2020-10-05 10:41:49 -0400

    Remove unnecessary declaration files

--- src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { R4 } from '@ahryman40k/ts-fhir-types';
-import { ValueSetMap } from './valueset';
-
-export * from './cql-exec-fhir';
-export * from './cql-execution';
-export * from './dataSource';
-export * from './valueset';
-export * from './window';
