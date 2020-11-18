6ea437fa4f140ac3d27ee8be0440fd79f1730190
Matthew Gramigna <mgramigna@mitre.org>
2020-11-18 12:23:29 -0500

    Update primitive logic to use initial value

--- src/utils/results-processing.ts
+++ src/utils/results-processing.ts
@@ -1,5 +1,21 @@
 import { R4 } from '@ahryman40k/ts-fhir-types';
 
+export enum Primitives {
+  STRING = 'valueString',
+  DATE = 'valueDate',
+  DATETIME = 'valueTime',
+  TIME = 'valueTime',
+  INT = 'valueInteger'
+}
+
+const ItemDataTypes = {
+  [Primitives.STRING]: R4.Questionnaire_ItemTypeKind._string,
+  [Primitives.INT]: R4.Questionnaire_ItemTypeKind._integer,
+  [Primitives.DATE]: R4.Questionnaire_ItemTypeKind._date,
+  [Primitives.DATETIME]: R4.Questionnaire_ItemTypeKind._dateTime,
+  [Primitives.TIME]: R4.Questionnaire_ItemTypeKind._time
+};
+
 // Filter results by querying proper data from the returned FHIR resources.
 // Modify the answerOptions of the questionnaire to include the results from execution
 export default function questionnaireUpdater(
@@ -14,8 +30,8 @@ export default function questionnaireUpdater(
     // Get Non-Empty Patient Results
     const igResources = cqlResults.patientResults[patientId];
     for (let key in igResources) {
-      let resourceList = igResources[key];
-      if ((Array.isArray(resourceList) && resourceList.length > 0) || (!Array.isArray(resourceList) && resourceList)) {
+      let cqlResult = igResources[key];
+      if ((Array.isArray(cqlResult) && cqlResult.length > 0) || (!Array.isArray(cqlResult) && cqlResult)) {
         // Find corresponding quesionnaire resource
         const matchingResource = questionnaireItems.find(
           element => getExpressionName(element) === key
@@ -24,15 +40,21 @@ export default function questionnaireUpdater(
 
         // Add answerOption element to questionnaire item
         let newAnswerOptions: R4.IQuestionnaire_AnswerOption[];
-        if (Array.isArray(resourceList)) {
-          newAnswerOptions = resourceList.map((r: any) => createAnswerOption(r, key));
+        if (isPrimitive(cqlResult)) {
+          const t = getPrimitiveType(cqlResult);
+          matchingResource.initial = [createPrimitiveInitialValue(t, cqlResult)];
+          matchingResource.type = ItemDataTypes[t];
         } else {
-          newAnswerOptions = [createAnswerOption(resourceList, key)];
-        }
-        if (matchingResource.answerOption === undefined || matchingResource.answerOption.length === 0) {
-          matchingResource.answerOption = newAnswerOptions;
-        } else {
-          matchingResource.answerOption = matchingResource.answerOption.concat(newAnswerOptions);
+          if (Array.isArray(cqlResult)) {
+            newAnswerOptions = cqlResult.map((r: any) => createAnswerOption(r, key));
+          } else {
+            newAnswerOptions = [createAnswerOption(cqlResult, key)];
+          }
+          if (matchingResource.answerOption === undefined || matchingResource.answerOption.length === 0) {
+            matchingResource.answerOption = newAnswerOptions;
+          } else {
+            matchingResource.answerOption = matchingResource.answerOption.concat(newAnswerOptions);
+          }
         }
         questionnaireItems[questionnaireItemIndex] = matchingResource;
       }
@@ -51,27 +73,46 @@ export function createAnswerOption(cqlResult: any, itemName?: string): R4.IQuest
   } else if (Array.isArray(cqlResult) && R4.RTTI_Coding.decode(cqlResult[0]._json).isRight()) {
     // Codings will come in as arrays
     return createValueCodingAnswerOptionCoding(cqlResult[0]._json as R4.ICoding);
-  } else if (typeof cqlResult === 'string') {
-    return createPrimitiveAnswerOption('valueString', cqlResult);
-  } else if (cqlResult.isDate) {
-    return createPrimitiveAnswerOption('valueDate', (cqlResult as Date).toString());
-  } else if (cqlResult.isDateTime) {
-    return createPrimitiveAnswerOption('valueTime', (cqlResult as Date).toString());
-  } else if (R4.RTTI_time.decode(cqlResult._json).isRight()) {
-    return createPrimitiveAnswerOption('valueTime', cqlResult as Date);
-  } else if (R4.RTTI_integer.decode(cqlResult).isRight()) {
-    return createPrimitiveAnswerOption('valueInteger', cqlResult as number);
   } else {
     throw new Error(`Unable to map cql result for ${itemName} to Questionnaire answerOption`);
   }
 }
 
-function createPrimitiveAnswerOption(
+export function isPrimitive(result: any): boolean {
+  return (
+    typeof result === 'string' ||
+    result.isDate ||
+    result.isDateTime ||
+    R4.RTTI_time.decode(result._json).isRight() ||
+    R4.RTTI_integer.decode(result).isRight()
+  );
+}
+
+export function getPrimitiveType(result: any): 'valueDate' | 'valueTime' | 'valueInteger' | 'valueString' {
+  if (typeof result === 'string') {
+    return Primitives.STRING;
+  } else if (result.isDate) {
+    return Primitives.DATE;
+  } else if (result.isDateTime) {
+    return Primitives.DATETIME;
+  } else if (R4.RTTI_time.decode(result._json).isRight()) {
+    return Primitives.TIME;
+  } else if (R4.RTTI_integer.decode(result).isRight()) {
+    return Primitives.INT;
+  } else {
+    throw new Error(`${result} was detected as primitive but could not be processed`);
+  }
+}
+
+export function createPrimitiveInitialValue(
   key: 'valueDate' | 'valueTime' | 'valueInteger' | 'valueString',
   cqlResult: Date | number | string
-): R4.IQuestionnaire_AnswerOption {
+): R4.IQuestionnaire_Initial {
   return {
-    [key]: cqlResult
+    [key]:
+      key === Primitives.DATE || key === Primitives.DATETIME || key === Primitives.TIME
+        ? cqlResult.toString()
+        : cqlResult
   };
 }
 
