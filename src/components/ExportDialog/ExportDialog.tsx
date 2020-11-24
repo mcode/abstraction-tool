7389cbc99e17617c70e2f358b6864422d5c07ffa
pmadria <pmadria@mitre.org>
2020-11-24 13:01:29 -0600

    pushing the button with content issue

---
+++ src/components/ExportDialog/ExportDialog.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { R4 } from '@ahryman40k/ts-fhir-types';
+import executeElm from '../../utils/cql-executor';
+import questionnaireUpdater from '../../utils/results-processing';
+import { ValueSetMap } from '../../types/valueset';
+import Modal from '@material-ui/core/Modal';
+import Dialog from '@material-ui/core/Dialog';
+import PropTypes from 'prop-types';
+import handleClickOpen from '../Abstractor/Abstractor'
+
+export interface Props {
+  open: boolean
+  close: (event: {}, reason: "backdropClick" | "escapeKeyDown") => void
+  content: { open: boolean; close: () => void; content: string | undefined; }
+}
+export default function ExportDialog({open, close, content}: Props) {
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={close}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        {content}
+      </Dialog>
+    </div>
+  );
+}
