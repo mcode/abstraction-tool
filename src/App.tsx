33ab3b4096964081b632117b76e59a7548bfe17b
Matthew Gramigna <mgramigna@mitre.org>
2020-11-30 10:57:06 -0500

    Add fallback component for handling errors and additional error throws

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { R4 } from '@ahryman40k/ts-fhir-types';
 import { usePatient } from './components/PatientProvider';
 import Abstractor from './components/Abstractor';
+import AppError from './components/AppError';
 import { QuestionnaireLoader } from './loaders/QuestionnaireLoader';
 import { LibraryLoader } from './loaders/libraryLoader';
 import { ValueSetLoader } from './loaders/ValueSetLoader';
@@ -25,41 +26,55 @@ const App = () => {
       // Load Questionnaire
       const questionnaireLoader = new QuestionnaireLoader();
       const url = './static/mcode-questionnaire.json';
-      try {
-        const questionnaireResource = await questionnaireLoader.getFromUrl(url);
-        setQuestionnaire(questionnaireResource);
+      const questionnaireResource = await questionnaireLoader.getFromUrl(url);
+      setQuestionnaire(questionnaireResource);
 
-        // Get FHIR Library
-        const extension = (questionnaireResource as R4.IQuestionnaire).extension?.find(
-          e => e.url === 'http://hl7.org/fhir/StructureDefinition/cqf-library'
+      // Get FHIR Library
+      const extension = (questionnaireResource as R4.IQuestionnaire).extension?.find(
+        e => e.url === 'http://hl7.org/fhir/StructureDefinition/cqf-library'
+      );
+
+      if (!extension || !extension.valueCanonical) {
+        throw new Error(
+          'Provided questionnaire does not contain a proper extension for "http://hl7.org/fhir/StructureDefinition/cqf-library"'
         );
-
-        if (extension && extension.valueCanonical) {
-          const response = await axios.get(extension.valueCanonical);
-          const fhirLibrary = response.data as R4.ILibrary;
-          const library = await new LibraryLoader(fhirLibrary).fetchELM();
-
-          const vsResponse = await axios.get('./static/mcode-valuesets.json');
-          const valueSetBundle = vsResponse.data as R4.IBundle;
-          const valueSetLoader = new ValueSetLoader(fhirLibrary, valueSetBundle);
-          const valueSetMap = await valueSetLoader.seedValueSets();
-
-          setLibrary(library);
-          setValueSetMap(valueSetMap);
-        }
-      } catch (e) {
-        console.error(`Error loading questionnaire data: ${e.message}`);
       }
+
+      const response = await axios.get(extension.valueCanonical);
+      const fhirLibrary = response.data as R4.ILibrary;
+
+      if (!R4.RTTI_Library.decode(fhirLibrary).isRight()) {
+        throw new Error(`${extension.valueCanonical} did not provide a valid FHIR library`);
+      }
+
+      const library = await new LibraryLoader(fhirLibrary).fetchELM();
+
+      const vsResponse = await axios.get('./static/mcode-valuesets.json');
+      const valueSetBundle = vsResponse.data as R4.IBundle;
+      const valueSetLoader = new ValueSetLoader(fhirLibrary, valueSetBundle);
+      const valueSetMap = await valueSetLoader.seedValueSets();
+
+      setLibrary(library);
+      setValueSetMap(valueSetMap);
     }
     if (patientData) {
       load();
     }
   }, [patientData]);
 
-  return patientData && questionnaire && library && valueSetMap ? (
-    <Abstractor patientData={patientData} questionnaire={questionnaire} library={library} valueSetMap={valueSetMap} />
-  ) : (
-    <p> Loading... </p>
+  return (
+    <AppError>
+      {patientData && questionnaire && library && valueSetMap ? (
+        <Abstractor
+          patientData={patientData}
+          questionnaire={questionnaire}
+          library={library}
+          valueSetMap={valueSetMap}
+        />
+      ) : (
+        <p> Loading... </p>
+      )}
+    </AppError>
   );
 };
 
