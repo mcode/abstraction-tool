d303c5187fdc85adf910d8aee87f882f7ad3567d
pmadria <65190574+pmadria@users.noreply.github.com>
2020-11-30 09:51:09 -0600

    Update src/components/ExportDialog/ExportDialog.tsx
    Co-authored-by: Mick O'Hanlon <mohanlon@mitre.org>

--- src/components/ExportDialog/ExportDialog.tsx
+++ src/components/ExportDialog/ExportDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Typography from '@material-ui/core/Typography';
 
