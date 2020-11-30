8185e2029a3dc91bcd6da3b0a31f7b7be3114a11
pmadria <pmadria@mitre.org>
2020-11-30 10:26:15 -0600

    added index.ts and changed the typography tag to pre and code

--- src/components/ExportDialog/ExportDialog.tsx
+++ src/components/ExportDialog/ExportDialog.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
-import Typography from '@material-ui/core/Typography';
 
 export interface Props {
   open: boolean
   close: (event: {}, reason: "backdropClick" | "escapeKeyDown") => void
   content: string | undefined
 }
-export default function ExportDialog({open, close, content}: Props) {
+const ExportDialog = ({open, close, content}: Props) => {
 
   return (
     <div>
@@ -25,10 +24,14 @@ export default function ExportDialog({open, close, content}: Props) {
           overflow: "scroll"
         }}
       >
-        <Typography display = "inline" variant="h6" align="center" paragraph gutterBottom>
+      <pre>
+        <code>
           {content}
-        </Typography>
+        </code>
+      </pre>
       </Dialog>
     </div>
   );
 }
+
+export default ExportDialog;
